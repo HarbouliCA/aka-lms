b277879e8f68cdd6ec8a6747b0938f37eb143c8a
Anass Harbouli <anass.harbouli.da@gmail.com>
2024-09-15 14:29:11 +0200

    CoursePriceAdded

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,13 +1,14 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import { LayoutDashboard } from "lucide-react";
+import { CircleDollarSign, LayoutDashboard, ListCheck } from "lucide-react";
 
 import { IconBadge } from "@/components/icon-badge";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
 import { CategoryForm } from "./_components/category-form";
+import { PriceForm } from "./_components/price-form";
 
 
 const CourseIdPage = async({
@@ -95,7 +96,33 @@ const CourseIdPage = async({
                         }))}
                     />
                 </div>
+                <div className="space-y-6">
+                    <div>
+                        <div className="flex items-center gap-x-2">
+                            <IconBadge icon={ListCheck} />
+                            <h1 className="text-xl">
+                                Course chapters
+                            </h1>
+                        </div>
+                        <div>
+                            TODO: Chapters
+                        </div>
+                    </div>
+                    <div>
+                        <div className="flex items-center gap-x-2">
+                            <IconBadge icon={CircleDollarSign} />
+                            <h2 className="text-xl">
+                                Sell your course
+                            </h2>
 
+                        </div>
+                        <PriceForm 
+                            initialData={course}
+                            courseId={course.id}                        
+                        />
+                    </div>
+                        
+                </div>
             </div>
         </div>
      );
