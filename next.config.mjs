448412d68f063ac45cca6446ae973ac4dbc78b58
Anass Harbouli <anass.harbouli.da@gmail.com>
2024-09-15 13:52:51 +0200

    CategoryFormAdded

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['utfs.io'], // Allow images from utfs.io
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',      // Specify the protocol (e.g., https)
+        hostname: 'utfs.io',    // Specify the hostname (e.g., utfs.io)
+        port: '',               // Optional: specify the port if any
+        pathname: '/**',        // Specify the pattern for the path (/** allows all paths)
+      },
+    ],
+  },
+};
+
+export default nextConfig;
