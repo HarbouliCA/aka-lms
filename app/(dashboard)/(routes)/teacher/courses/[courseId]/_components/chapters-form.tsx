6a7add306e2ffc7e040b46ee8ecbb96f276f36ea
Anass Harbouli <anass.harbouli.da@gmail.com>
2024-09-17 20:08:18 +0200

    PrismaSchema&ChapterCreationApiEnded

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -0,0 +1,136 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {useForm} from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { Pencil, PlusCircle } from "lucide-react";
+import { useState } from "react";
+import { FormProvider } from "react-hook-form"; // Import this for context
+import { useRouter } from "next/navigation";
+
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input";
+import toast from "react-hot-toast";
+import { cn } from "@/lib/utils";
+import { Chapter, Course } from "@prisma/client";
+import { ChaptersList } from "./chapters-list";
+
+
+interface ChaptersFormProps {
+    initialData: Course & {chapters: Chapter[]};
+    courseId : string;
+};
+
+const formSchema = z.object({
+    title : z.string().min(1),
+});
+
+export const ChaptersForm = ({
+    initialData,
+    courseId
+}: ChaptersFormProps ) => {
+    const [isCreating, setIsCreating] = useState(false);
+
+    const [isUpdating, setIsUpdating] = useState(false);
+
+    const toggleCreating = () => {
+        setIsCreating((current) => !current);
+    }
+
+    const router = useRouter();
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            title: "",
+        },
+    });
+
+    const { isSubmitting, isValid } = form.formState;
+
+    const onSumbit = async (values: z.infer<typeof formSchema>) => {
+       try{
+        await axios.post(`/api/courses/${courseId}/chapters`, values);
+        toast.success("Chapter updated");
+        toggleCreating();
+        router.refresh();
+       } catch{
+        toast.error("something went wrong")
+       }
+    }
+
+    return ( 
+        <div className="mt-6 border bg-slate-100 rounder-md p-4">
+            <div className="font-medium flex items-center justify-between">
+                Course chapters
+                <Button onClick={toggleCreating} variant="ghost" >
+                    {isCreating ? (
+                        <>Cancel</>
+                    ) : (
+                    <>
+                    <PlusCircle className="h-4 w-4 mr-2" />
+                    Add a chapter
+                    </>
+                    )}
+                </Button>
+            </div>
+            {isCreating && (
+                <FormProvider {...form}> {/* Add FormProvider to wrap the form context */}
+                <form
+                    onSubmit={form.handleSubmit(onSumbit)}
+                    className="space-y-4 mt-4"
+                >
+                    <FormField 
+                        control={form.control}
+                        name = "title"
+                        render = {({ field }) => (
+                            <FormItem>
+                                <FormControl>
+                                    <Input
+                                    disabled = {isSubmitting}
+                                    placeholder="e.g 'This course is about...'"
+                                    {...field}
+                                    />  
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                        />
+                        <Button
+                            type="submit"
+                            disabled={!isValid || isSubmitting}
+                        >
+                            Create
+                        </Button>
+                </form>
+                </FormProvider>
+            )}
+            {!isCreating && (
+                <div className={cn(
+                    "text-sm mt-2",
+                    !initialData.chapters.length && "text-slate-500 italic"
+                )}>
+                {!initialData.chapters.length && "No chapters"}
+                <ChaptersList
+                    onEdit = {() => {}}
+                    onReorder = {() => {}}
+                    items = {initialData.chapters || []}
+                />
+                </div>
+            )}
+            {!isCreating && (
+                <p className="text-xs text-muted-foreground mt-4">
+                    Drag and drop to reorder the chapters
+                </p>
+            )}
+        </div>
+     )
+}
