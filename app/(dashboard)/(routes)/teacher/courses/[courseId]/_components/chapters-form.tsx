362e91c3e5e77b21ce3be1149d12dd0d1e564645
Anass Harbouli <anass.harbouli.da@gmail.com>
2024-09-21 14:08:39 +0200

    ChapterReorderAdded

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {useForm} from "react-hook-form";
 import { Button } from "@/components/ui/button";
-import { Pencil, PlusCircle } from "lucide-react";
+import { Loader2, Pencil, PlusCircle } from "lucide-react";
 import { useState } from "react";
 import { FormProvider } from "react-hook-form"; // Import this for context
 import { useRouter } from "next/navigation";
@@ -67,8 +67,33 @@ export const ChaptersForm = ({
        }
     }
 
+    const onReorder = async(updateData : {id: string; position: number}[]) =>{
+        try{
+            setIsUpdating(true)
+
+            await axios.put(`/api/courses/${courseId}/chapters/reorder`, {
+                list: updateData
+            });
+            toast.success("Chapters reordered");
+            router.refresh();
+        }catch{
+            toast.error("Something went wrong");
+        } finally{
+            setIsUpdating(false);
+        }
+    }
+
+    const onEdit = ( id : string ) => {
+        router.push(`/teacher/courses/${courseId}/chapters/${id}`);
+    }
+
     return ( 
-        <div className="mt-6 border bg-slate-100 rounder-md p-4">
+        <div className="relative mt-6 border bg-slate-100 rounder-md p-4">
+            {isUpdating && (
+                <div className="absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-m flex items-center justify-center">
+                    <Loader2 className="animate-spin h-6 w-6 text-sky-700" />
+                </div>
+            )}
             <div className="font-medium flex items-center justify-between">
                 Course chapters
                 <Button onClick={toggleCreating} variant="ghost" >
@@ -120,8 +145,8 @@ export const ChaptersForm = ({
                 )}>
                 {!initialData.chapters.length && "No chapters"}
                 <ChaptersList
-                    onEdit = {() => {}}
-                    onReorder = {() => {}}
+                    onEdit = {onEdit}
+                    onReorder = {onReorder}
                     items = {initialData.chapters || []}
                 />
                 </div>
